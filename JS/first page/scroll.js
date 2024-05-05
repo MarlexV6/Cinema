5acd63b456414369287e9e2cd2f257a42350bf94
Archi <maa20064@gmail.com>
2024-05-06 01:34:28 +0300

    Сохранено

---
+++ JS/first page/scroll.js
@@ -0,0 +1,65 @@
+
+let items = [];
+
+function loadXML(url) {
+    return fetch(url)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then(data => {
+            items = Array.from(data.querySelectorAll('movie')).map(item => {
+                return {
+                    age: item.querySelector('age').textContent,
+                    image: item.querySelector('image').textContent,
+                    name: item.querySelector('name').textContent,
+                    tags: item.querySelector('tags').textContent,
+                    description: item.querySelector('description').textContent,
+                    time: item.querySelector('time').textContent,
+                    trailer: item.querySelector('trailer').textContents
+
+                };
+            });
+            return items;
+        });
+}
+function addItemsToDOM(items) {
+    const itemsList = document.querySelector('#moviesContainer');
+    items.forEach(item => {
+        const itemDiv = document.createElement('div');
+        console.log(itemDiv);
+        itemDiv.className= 'movie';
+        itemDiv.innerHTML = `
+            <img src="${item.image}">
+            <h3>${item.age}</h3>
+            <h2>${item.name}</h2>
+            <p>${item.tags}</p>
+
+            <a href="/HTML/MoviePage.html?name=${encodeURIComponent(item.name)}
+            &age=${encodeURIComponent(item.age)}
+            &image=${encodeURIComponent(item.image)}
+            &tags=${encodeURIComponent(item.tags)}
+            &description=${encodeURIComponent(item.description)}
+            &time=${encodeURIComponent(item.time)}
+            &trailer=${encodeURIComponent(item.trailer)}">Купить билет</a>
+        `;
+        itemsList.appendChild(itemDiv);
+        
+    });
+}
+document.addEventListener('DOMContentLoaded', function() {
+loadXML('/XML/movies.xml')
+
+addItemsToDOM(items)
+});
+
+document.addEventListener('DOMContentLoaded', function() 
+{
+    loadXML('/XML/movies.xml')
+    .then(items => addItemsToDOM(items))
+    .catch(error => console.error('Ошибка при загрузке XML:', error));
+});
+
+
+
+
+
+
---
+++ JS/first page/scrolling.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const carousel = document.querySelector('.carousel');
+    let slideIndex = 0;
+    const slides = carousel.querySelectorAll('.carousel-slide');
+    const slideCount = slides.length;
+    let timerId = null;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${(i - index) * 100}%)`;
+        });
+    }
+    function nextSlide() {
+        slideIndex = (slideIndex + 1) % slideCount;
+        showSlide(slideIndex);
+ 
+        if (timerId) {
+            clearInterval(timerId);
+        }
+        timerId = setInterval(nextSlide, 3000000);
+    }
+    function prevSlide() {
+        slideIndex = (slideIndex - 1 + slideCount) % slideCount; 
+        showSlide(slideIndex);
+
+        if (timerId) {
+            clearInterval(timerId);
+        }
+        timerId = setInterval(nextSlide, 3000000); 
+    }
+    showSlide(slideIndex);
+    timerId = setInterval(nextSlide, 3000000); 
+     document.querySelector('#next-slide-button').addEventListener('click', nextSlide);
+     document.querySelector('#prev-slide-button').addEventListener('click', prevSlide);
+});
+
+
+
+
